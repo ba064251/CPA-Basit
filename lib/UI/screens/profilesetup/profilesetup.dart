89e265af5c4dd39b005ba60ac6219e444f1e5cf4
Basit Ali <ba064251@gmail.com>
2023-11-21 07:12:50 +0500

    Icon Done -> Search, CPA 7 -> Tab bar

---
+++ lib/UI/screens/profilesetup/profilesetup.dart
@@ -0,0 +1,135 @@
+import 'dart:io';
+
+import 'package:cpa/UI/screens/profilesetup/widgets/dob.dart';
+import 'package:cpa/data/dummy.dart';
+import 'package:cpa/export.dart';
+import 'package:cpa/services/auth_services.dart';
+import 'package:cpa/utils/country_code_picker.dart';
+import 'package:cpa/utils/gender_picker.dart';
+
+import 'widgets/local_image_screen.dart';
+
+class ProfileSetup extends StatefulWidget {
+  final String uid;
+  const ProfileSetup({
+    super.key,
+    required this.uid,
+  });
+
+  @override
+  State<ProfileSetup> createState() => _ProfileSetupState();
+}
+
+class _ProfileSetupState extends State<ProfileSetup> {
+  bool isSignUp = false;
+
+  TextEditingController _nameController = TextEditingController();
+
+  final TextEditingController _nickNameController = TextEditingController();
+
+  final TextEditingController _dateController = TextEditingController();
+  final TextEditingController _phoneController = TextEditingController();
+
+  @override
+  void initState() {
+    _nameController = TextEditingController(
+      text: fName,
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.arrow_back_ios),
+                    ),
+                    SizedBoxes.horizontalExtraGargangua,
+                    SizedBoxes.horizontalExtraGargangua,
+                    SizedBoxes.horizontalBig,
+                    Align(
+                      alignment: Alignment.topCenter,
+                      child: Text(
+                        'Signup',
+                        textAlign: TextAlign.center,
+                        style: CPATextTheme().small.copyWith(
+                            color: CPAColorTheme().black,
+                            fontSize: 18,
+                            fontFamily: 'Red Hat Display',
+                            fontWeight: FontWeight.w600,
+                            height: 0),
+                      ),
+                    )
+                  ],
+                ),
+                Center(
+                  child: LocalImagePicker(),
+                ),
+                SizedBoxes.verticalBig,
+                CustomTextField(controller: _nameController, hintText: 'Name'),
+                SizedBoxes.verticalMedium,
+                CustomTextField(
+                    controller: _nickNameController, hintText: 'Nickname'),
+                SizedBoxes.verticalMedium,
+                DateOfBirthWidget(dateController: _dateController),
+                SizedBoxes.verticalMedium,
+                PhoneDropdown(phoneController: _phoneController),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: GenderDropdown(),
+                ),
+                SizedBoxes.verticalExtraGargangua,
+                SizedBoxes.verticalExtraGargangua,
+                SizedBoxes.verticalExtraGargangua,
+                SizedBoxes.verticalExtraGargangua,
+                CustomButton(
+                    buttonText: 'Continue',
+                    onPressed: () async {
+                      print(
+                          "___________________________________URL______________");
+
+                      print(profileImg);
+                      if (profileImg != null) {
+                        File profile = File(profileImg!);
+
+                        await AuthServices.uploadImage(profile, widget.uid);
+                      }
+                      print("________________________________________________");
+                      print(imgUrl);
+                      print("________________________________________________");
+                      uid = widget.uid;
+                      await AuthServices.addUser(
+                        name: _nameController.text.toString(),
+                        nickname: _nickNameController.text.toString(),
+                        dob: _dateController.text.toString(),
+                        uid: widget.uid,
+                        gender: gender,
+                        imgUrl: imgUrl,
+                        number: _phoneController.text.toString(),
+                      );
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const HomeScreen(),
+                        ),
+                      );
+                    })
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import "package:cpa/UI/screens/splashscreen/mainSplash.dart";
+import "package:cpa/api/api_service.dart";
+import "package:firebase_core/firebase_core.dart";
+import "package:flutter/material.dart";
+
+import "UI/screens/Analysis/analysis_final.dart";
+import "UI/screens/Analysis/analysis_page1.dart";
+import "UI/screens/homescreen/homescreen.dart";
+import "UI/screens/homescreen/products/products_screen.dart";
+import "UI/screens/login.dart";
+import "UI/screens/splashscreen/splash_screen.dart";
+import "firebase_options.dart";
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ProductsHome(),
+    );
+  }
+}
